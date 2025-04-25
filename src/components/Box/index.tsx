163e139016aaa494cd2c5c73adafd28419868ceb
Lucas <lucasoliveirars22@gmail.com>
2025-04-25 15:22:43 -0300

    feat: NewTask and Box components

---
+++ src/components/Box/index.tsx
@@ -0,0 +1,41 @@
+import { SquarePen } from "lucide-react";
+import { Trash2 } from "lucide-react";
+
+interface BoxProps {
+  text: string;
+  description: string;
+}
+
+export default function Box({ text, description }: BoxProps) {
+  return (
+    <>
+      <div className="w-[485px] h-[150px] border-gray-300 border-1 rounded-lg p-4 space-y-2">
+        <div className="flex justify-between items-start">
+          <div className="flex items-center gap-2 pb-5">
+            <input type="checkbox" className="size-5 accent-black" />
+            <h3 className="font-semibold text-base">{text}</h3>
+          </div>
+
+          <div className="flex items-center gap-5">
+            <button
+              type="button"
+              className="cursor-pointer text-gray-500 hover:text-black"
+            >
+              <SquarePen />
+            </button>
+            <button
+              type="button"
+              className="cursor-pointer text-gray-500 hover:text-black"
+            >
+              <Trash2 />
+            </button>
+          </div>
+        </div>
+
+        <div>
+          <p className="text-sm text-gray-500">{description}</p>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/NewTask/index.tsx
@@ -0,0 +1,28 @@
+import { Plus } from "lucide-react";
+
+export default function NewTask() {
+  return (
+    <>
+      <div className="w-[1000px] h-[300px] border-gray-300 border-1 rounded-lg p-4 space-y-4">
+        <h2 className="text-xl font-bold text-gray-900">Adicionar tarefa</h2>
+        <div className="space-y-3">
+          <form action="#" className="space-y-5">
+            <input
+              type="text"
+              className="w-full px-3 py-2 border-gray-300 rounded-md outline-none ring-1 ring-gray-300"
+              placeholder="Título da tarefa"
+            />
+            <textarea
+              className="w-full h-[100px] px-3 py-2 border-gray-300 rounded-md outline-none ring-1 ring-gray-300"
+              placeholder="Descrição (opcional)"
+            />
+
+            <button className="flex items-center justify-center w-full bg-gray-800 text-white font-medium py-2 rounded-md gap-2 cursor-pointer">
+              <Plus /> Adicionar tarefa
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
